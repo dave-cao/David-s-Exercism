225583af629df76ab3f7b0a7d34c88990c60f9f6
David Cao <sirdavidcao@gmail.com>
2023-04-20 14:12:37 -0600

    work on triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,5 +1,21 @@
-def equilateral(sides):
-    pass
+"""Exercism Python: Triangle
+
+Problem:
+- Determine if a triangle is equilateral, isosceles, or scalene.
+
+1. Equilateral: all three sides the same length
+2. Isolsceles: at least two sides are the same length
+3. Scalene: all sides are different length
+
+Caveats: 
+- for a shape to be a triangle, all sides must have a length > 0, and the sum
+    of the lengths of any two sides must be greater than or equal to the
+    length of the third side.
+"""
+
+
+def equilateral(sides: list[int]) -> bool:
+    return all(side == sides[0] for side in sides)
 
 
 def isosceles(sides):
@@ -8,3 +24,6 @@ def isosceles(sides):
 
 def scalene(sides):
     pass
+
+
+equilateral([3, 2, 2])
